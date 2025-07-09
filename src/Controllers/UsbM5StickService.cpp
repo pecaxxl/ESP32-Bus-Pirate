23c1eaa1182279d864babe61a96733483a671848
Geo <tp53.geo@gmail.com>
2025-07-09 07:04:06 +0200

    minor fix on usb controller for stick

--- src/Controllers/UsbM5StickService.cpp
+++ src/Controllers/UsbM5StickService.cpp
@@ -6,11 +6,13 @@ UsbM5StickController::UsbM5StickController(ITerminalView& terminalView)
     : terminalView(terminalView) {}
 
 void UsbM5StickController::handleCommand(const TerminalCommand&) {
-    terminalView.print("\n[INFO] USB is not supported on M5StickC Plus 2.\n\n");
+    terminalView.println("\n[INFO] USB is not supported on M5StickC Plus 2.");
+    terminalView.println("");
 }
 
 void UsbM5StickController::ensureConfigured() {
-    terminalView.print("\n[INFO] Skipping USB not supported on M5StickC Plus 2.\n\n");
+    terminalView.println("\n[INFO] Skipping USB not supported on M5StickC Plus 2.\n");
+    terminalView.println("");
 }
 
 #endif
