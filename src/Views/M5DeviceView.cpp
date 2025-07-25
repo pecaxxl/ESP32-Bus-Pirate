8d037ff9944384e2810942b326674e79c9ff347e
Geo <tp53.geo@gmail.com>
2025-07-25 16:31:32 +0200

    change mentions to 0.3 version

--- src/Views/M5DeviceView.cpp
+++ src/Views/M5DeviceView.cpp
@@ -29,7 +29,7 @@ void M5DeviceView::logo() {
     M5.Lcd.setTextSize(1.6);
     M5.Lcd.setCursor(43, 80);
     M5.Lcd.setTextColor(TEXT_COLOR);
-    M5.Lcd.printf("Version 0.2 - Geo");
+    M5.Lcd.printf("Version 0.3 - Geo");
 
     delay(3000);
 }
